9c55a645c0180dbb63ebc74bef72c84cbe03c78e
Ben Hughes <ben@railsgarden.com>
2010-12-29 15:07:25 -0500

    Initial Rails 3 application.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Jazzity::Application.config.secret_token = '8254c9a1530d7d7a69d34987dabd93df7e8482a0dc78d53e73b1a21add71abc878347c00f1432d8cc609bb0b41fdc6ca8a1d5f5eb3f4d44dd093b5ac3f39e3fe'
