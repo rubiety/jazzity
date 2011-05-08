b6bd5de53e8150dc3657bec81d11c6114c9f4940
Ben Hughes <ben@railsgarden.com>
2011-05-08 09:40:35 -0700

    Removed old authentication and user models.

--- app/models/user.rb
+++
@@ -1,8 +0,0 @@
-class User < ActiveRecord::Base
-  has_many :authentications
-
-  devise :database_authenticatable, :registerable, :recoverable, :rememberable, :trackable, :validatable
-
-  attr_accessible :email, :password, :password_confirmation, :remember_me
-  
-end
