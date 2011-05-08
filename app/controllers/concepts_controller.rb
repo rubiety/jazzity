fd65f08d4a39b65361a9767d62c02986a97ef111
Ben Hughes <ben@railsgarden.com>
2011-05-08 12:02:37 -0700

    Added initial concepts index.

--- app/controllers/concepts_controller.rb
+++ app/controllers/concepts_controller.rb
@@ -1,7 +1,17 @@
 class ConceptsController < ApplicationController
+  before_filter :find_concept, :except => [:index, :new, :create]
+
   def index
+    @concepts = Concept.all
   end
 
   def show
+  end
+
+
+  protected
+
+  def find_concept
+    @concept = Concept.find(params[:id])
   end
 end
