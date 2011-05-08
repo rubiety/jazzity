22ca891acab70afb6286363f50bc49c876b3ed60
Ben Hughes <ben@railsgarden.com>
2011-05-07 23:12:57 -0700

    Initial setup of jazzity for styles.

--- Gemfile
+++ Gemfile
@@ -1,6 +1,6 @@
 source "http://rubygems.org"
 
-gem "rails", "3.0.6"
+gem "rails", "3.0.7"
 gem "mysql2", "~> 0.2.7"
 
 gem "haml"
--- config/routes.rb
+++ config/routes.rb
@@ -12,18 +12,19 @@ Jazzity::Application.routes.draw do
     end
   end
   
-  resources :concepts
-  resources :musicians
   resources :tunes
+  resources :musicians
   resources :chord_qualities
   resources :chords
-  resources :voicings
-  resources :progressions
-  resources :notes_collections, :as => "notes"
   resources :scales do
     resources :modes
   end
+
+  resources :voicings
+  resources :progressions
+  resources :concepts
+  resources :notes_collections, :as => "notes"
   
-  root :to => "dashboards#index"
+  root :to => "dashboards#show"
   
 end
