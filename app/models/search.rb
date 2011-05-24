626c59d2cc9f6449b6be05e72d3d01d9ea608e72
Ben Hughes <ben@railsgarden.com>
2011-05-23 22:36:47 -0700

    Implemented search key pre-emption with 'in A minor' suffix, for example.

--- app/models/search.rb
+++ app/models/search.rb
@@ -4,25 +4,53 @@ class Search
   end
   
   def all
-    [Chord, ChordQuality, Concept, Form, Instrument, Meter, Mode, Progression, 
+    extract_key(@query)
+    returned = results(@query).flatten.compact
+    returned = normalize_results(returned)
+
+    add_key_context(returned)
+  end
+
+
+  protected
+
+  def results(query)
+    [
+      Chord, ChordQuality, Concept, Form, Instrument, Meter, Mode, Progression, 
       Scale, Tune, Vehicle, Voicing, NoteSequence, ChordSequence, ChordNoteSequence
-    ].map do |klass|
-      ActiveRecord::Base.logger.info "Trying '#{@query}' for #{klass.name}..."
-      klass[@query]
-    end.flatten.compact.map do |result|
+    ].map {|klass| klass[query] }
+  end
+
+  def normalize_results(results)
+    results.map do |result|
       if result.is_a?(Mode)
         [result.scale, result]
       else
         result
       end
-    end.map do |result|
+    end
+  end
+
+  def add_key_context(results)
+    results.map do |result|
       last_piece = [result].flatten.last
 
-      if last_piece.respond_to?(:key) and last_piece.key
-        ([last_piece.key] + [result].flatten)
+      if last_piece.respond_to?(:key) and (@key || last_piece.key)
+        ([@key || last_piece.key] + [result].flatten)
       else
         result
       end
+    end
+  end
+
+  def extract_key(query)
+    query.gsub!(/(major|minor)$/i, "").to_s.strip!.to_s
+
+    if match = /in ([a-gA-G](b|#)?)$/.match(query)
+      query.gsub!(/in ([a-gA-G](b|#)?)$/, "").to_s.strip!.to_s
+      @key = Key[match[1]]
+    else
+      nil
     end
   end
 end
