37fb8bd8d42355ceaa3662647f251e20de91c67e
Ben Hughes <ben@railsgarden.com>
2011-05-30 19:30:43 -0700

    Using compass standalone to evade current issues with Rails 3.1 asset pipelining.

--- config/compass.rb
+++ config/compass.rb
@@ -1,13 +1,36 @@
-# This configuration file works with both the Compass command line tool and within Rails.
-require 'html5-boilerplate'
-# Require any additional compass plugins here.
+# BEFORE COMPASS WATCH:
+#
+# # This configuration file works with both the Compass command line tool and within Rails.
+# require 'html5-boilerplate'
+# # Require any additional compass plugins here.
+# 
+# project_type = :rails
+# project_path = Compass::AppIntegration::Rails.root
+# # Set this to the root of your project when deployed:
+# http_path = "/"
+# css_dir = "public/stylesheets"
+# sass_dir = "app/stylesheets"
+# environment = Compass::AppIntegration::Rails.env
+# # To enable relative paths to assets via compass helper functions. Uncomment:
+# # relative_assets = true
 
-project_type = :rails
-project_path = Compass::AppIntegration::Rails.root
+
+# TEMPORARY:
 # Set this to the root of your project when deployed:
-http_path = "/"
-css_dir = "public/stylesheets"
-sass_dir = "app/stylesheets"
-environment = Compass::AppIntegration::Rails.env
-# To enable relative paths to assets via compass helper functions. Uncomment:
-# relative_assets = true
+require "html5-boilerplate"
+require "fancy-buttons"
+
+project_type      = :stand_alone
+
+http_path         = "/assets/"
+
+css_dir           = "app/assets/stylesheets/"
+sass_dir          = "app/compass/"
+images_dir        = "app/assets/images/"
+http_images_path  = "/assets/"
+javascripts_dir   = "js"
+
+line_comments     = true
+project_type      = :stand_alone # can be :stand_alone or :rails.
+output_style      = :expanded # :nested, :expanded, :compact, or :compressed.
+relative_assets   = false
