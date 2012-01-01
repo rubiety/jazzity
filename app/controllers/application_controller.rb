6dc7595815996c633191680758dcd55e1fdfecf3
Ben Hughes <ben@railsgarden.com>
2012-01-01 14:11:29 -0500

    Fixed issue with FB not redirecting back to original location.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -22,6 +22,13 @@ class ApplicationController < ActionController::Base
     end
   end
 
+  def after_sign_in_path_for(resource_or_scope)
+    if request.env['omniauth.origin']
+      request.env['omniauth.origin']
+    end
+  end
+  
+
 
   protected
 
