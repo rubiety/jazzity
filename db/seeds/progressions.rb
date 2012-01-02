c66e070295d94a996f1800d09cd76a60d4bfef55
Ben Hughes <ben@railsgarden.com>
2012-01-02 13:49:55 -0500

    Added container styling on top of black linen background pattern.

--- db/seeds/progressions.rb
+++ db/seeds/progressions.rb
@@ -1,12 +1,12 @@
 ProgressionFamily.create!(:name => "Basic Progressions").tap do |family|
-  family.progressions.create!(:name => "Major ii-V", :full_tune => false,  :summary => "The most commonly-used progression in Jazz.", :information => "").tap do |p|
+  family.progressions.create!(:name => "Major ii-V", :seeding => true, :full_tune => false,  :summary => "The most commonly-used progression in Jazz.", :information => "").tap do |p|
     p.components.create([
       {:position => 1, :index => 2, :chord => Chord["min7"]},
       {:position => 2, :index => 7, :chord => Chord["7"]}
     ])
   end
 
-  family.progressions.create!(:name => "Major ii-V-I", :full_tune => false,  :summary => "The most commonly-used progression in Jazz.", :information => "").tap do |p|
+  family.progressions.create!(:name => "Major ii-V-I", :seeding => true, :full_tune => false,  :summary => "The most commonly-used progression in Jazz.", :information => "").tap do |p|
     p.components.create([
       {:position => 1, :index => 2, :chord => Chord["min7"]},
       {:position => 2, :index => 7, :chord => Chord["7"]},
@@ -14,14 +14,14 @@ ProgressionFamily.create!(:name => "Basic Progressions").tap do |family|
     ])
   end
 
-  family.progressions.create!(:name => "Minor ii-V", :full_tune => false,  :summary => "The most commonly-used progression in Jazz.", :information => "").tap do |p|
+  family.progressions.create!(:name => "Minor ii-V", :seeding => true, :full_tune => false,  :summary => "The most commonly-used progression in Jazz.", :information => "").tap do |p|
     p.components.create([
       {:position => 1, :index => 2, :chord => Chord["dim7"]},
       {:position => 2, :index => 7, :chord => Chord["alt"]}
     ])
   end
 
-  family.progressions.create!(:name => "Minor ii-V-I", :full_tune => false,  :summary => "The most commonly-used progression in Jazz.", :information => "").tap do |p|
+  family.progressions.create!(:name => "Minor ii-V-I", :seeding => true, :full_tune => false,  :summary => "The most commonly-used progression in Jazz.", :information => "").tap do |p|
     p.components.create([
       {:position => 1, :index => 2, :chord => Chord["dim7"]},
       {:position => 2, :index => 7, :chord => Chord["alt"]},
@@ -32,7 +32,7 @@ end
 
 
 ProgressionFamily.create!(:name => "Turnarounds").tap do |family|
-  family.progressions.create!(:name => "Major I-iv-ii-V", :full_tune => false,  :summary => "A typical turn-around at the end of a major tune.", :information => "").tap do |p|
+  family.progressions.create!(:name => "Major I-iv-ii-V", :seeding => true, :full_tune => false,  :summary => "A typical turn-around at the end of a major tune.", :information => "").tap do |p|
     p.components.create([
       {:position => 1, :index => 0, :chord => Chord["maj7"]},
       {:position => 2, :index => 9, :chord => Chord["7"]},
@@ -41,7 +41,7 @@ ProgressionFamily.create!(:name => "Turnarounds").tap do |family|
     ])
   end
 
-  family.progressions.create!(:name => "Major iii-iv-ii-V", :full_tune => false,  :summary => "A typical turn-around at the end of a major tune.", :information => "").tap do |p|
+  family.progressions.create!(:name => "Major iii-iv-ii-V", :seeding => true, :full_tune => false,  :summary => "A typical turn-around at the end of a major tune.", :information => "").tap do |p|
     p.components.create([
       {:position => 1, :index => 4, :chord => Chord["min7"]},
       {:position => 2, :index => 9, :chord => Chord["7"]},
@@ -50,7 +50,7 @@ ProgressionFamily.create!(:name => "Turnarounds").tap do |family|
     ])
   end
 
-  family.progressions.create!(:name => "Chromatic b3-2-b2-I", :full_tune => false,  :summary => "A typical turn-around at the end of a major tune.", :information => "").tap do |p|
+  family.progressions.create!(:name => "Chromatic b3-2-b2-I", :seeding => true, :full_tune => false,  :summary => "A typical turn-around at the end of a major tune.", :information => "").tap do |p|
     p.components.create([
       {:position => 1, :index => 3, :chord => Chord["7alt"]},
       {:position => 2, :index => 2, :chord => Chord["min7"]},
@@ -62,7 +62,7 @@ end
 
 
 ProgressionFamily.create!(:name => "Blues").tap do |family|
-  family.progressions.create!(:name => "Basic Blues", :bars => 12, :full_tune => true, :form => Form['A-B-C'], :summary => "The most commonly-played progression in Jazz, this 12-bar form has many variations.", :information => "").tap do |p|
+  family.progressions.create!(:name => "Basic Blues", :seeding => true, :bars => 12, :full_tune => true, :form => Form['A-B-C'], :summary => "The most commonly-played progression in Jazz, this 12-bar form has many variations.", :information => "").tap do |p|
     p.components.create([
       {:position => 1, :index => 0, :chord => Chord["7"]},
       {:position => 3, :index => 5, :chord => Chord["7"]},
@@ -78,7 +78,7 @@ ProgressionFamily.create!(:name => "Blues").tap do |family|
       {:position => 23, :index => 0, :chord => Chord["7"]}
     ])
 
-    p.variants.create!(:progression_family => family, :name => "Blues 2", :bars => 12, :full_tune => true, :form => Form['A-B-C']).tap do |pp|
+    p.variants.create!(:progression_family => family, :name => "Blues 2", :seeding => true, :bars => 12, :full_tune => true, :form => Form['A-B-C']).tap do |pp|
       pp.components.create([
         {:position => 1, :index => 0, :chord => Chord["7"]},
         {:position => 3, :index => 5, :chord => Chord["7"]},
@@ -95,7 +95,7 @@ ProgressionFamily.create!(:name => "Blues").tap do |family|
       ])
     end
 
-    p.variants.create!(:progression_family => family, :name => "Jazz Blues", :bars => 12, :full_tune => true, :form => Form['A-B-C']).tap do |pp|
+    p.variants.create!(:progression_family => family, :name => "Jazz Blues", :seeding => true, :bars => 12, :full_tune => true, :form => Form['A-B-C']).tap do |pp|
       pp.components.create([
         {:position => 1, :index => 0, :chord => Chord["7"]},
         {:position => 3, :index => 5, :chord => Chord["7"]},
@@ -112,7 +112,7 @@ ProgressionFamily.create!(:name => "Blues").tap do |family|
       ])
     end
 
-    p.variants.create!(:progression_family => family, :name => "Blues 4", :bars => 12, :full_tune => true, :form => Form['A-B-C']).tap do |pp|
+    p.variants.create!(:progression_family => family, :name => "Blues 4", :seeding => true, :bars => 12, :full_tune => true, :form => Form['A-B-C']).tap do |pp|
       pp.components.create([
         {:position => 1, :index => 0, :chord => Chord["7"]},
         {:position => 3, :index => 5, :chord => Chord["7"]},
@@ -136,7 +136,7 @@ ProgressionFamily.create!(:name => "Blues").tap do |family|
 end
 
 ProgressionFamily.create(:name => "Rhythm Changes").tap do |family|
-  family.progressions.create!(:name => "Original Rhythm Changes", :bars => 32, :full_tune => true, :form => Form['A-A-B-A'], 
+  family.progressions.create!(:name => "Original Rhythm Changes", :seeding => true, :bars => 32, :full_tune => true, :form => Form['A-A-B-A'], 
     :summary => "The second most commonly-played progression in Jazz, this A-A-B-A progression borrows from the famous tune, I've Got Rhythm.",
     :information => ""
   )
