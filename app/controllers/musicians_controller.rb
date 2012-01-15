68cd7a30071f420e28527240113c25959ca202a3
Ben Hughes <ben@railsgarden.com>
2012-01-15 13:46:12 -0800

    Minor refactoring on musicians searching.

--- app/controllers/musicians_controller.rb
+++ app/controllers/musicians_controller.rb
@@ -1,12 +1,13 @@
 class MusiciansController < ApplicationController
   before_filter :find_musician, :except => [:index, :search, :new, :create]
+  before_filter :find_filters, :only => [:index]
 
   respond_to :html, :json
 
   def index
     @musicians = Musician.order(:prominence).page(params[:page])
     @musicians = @musicians.search(params[:query]) if params[:query].present?
-    @musicians = @musicians.instrument(Instrument.find(params[:instrument])) if params[:instrument].present?
+    @musicians = @musicians.instrument(@instrument) if @instrumnet
     @musicians = @musicians.famous if params[:show] == "musicians" or params[:show].nil?
     @musicians = @musicians.members if params[:show] == "members"
 
@@ -25,5 +26,9 @@ class MusiciansController < ApplicationController
 
   def find_musician
     @musician = Musician.find(params[:id])
+  end
+
+  def find_filters
+    @instrument = Instrument.find(params[:instrument]) if params[:instrument].present?
   end
 end
