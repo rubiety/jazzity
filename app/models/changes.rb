d76b634d0eea5a175facd9811abc5cc8d0a9f8fb
Ben Hughes <ben@railsgarden.com>
2012-04-16 12:12:35 -0400

    Minor fixes to Jazzity' changes implementation.

--- app/models/changes.rb
+++ app/models/changes.rb
@@ -28,16 +28,20 @@ class Changes
       @letter = data[:section]
       @time = data[:time]
       @repeat = data[:repeat]
-      @bars = data[:bars]
+      @bars = normalize_bars(data[:bars])
+    end
+
+    def normalize_bars(raw_bars)
+      raw_bars.map do |raw_bar|
+        raw_bar.is_a?(Array) && raw_bar.size == 1 ? raw_bar.first : raw_bar
+      end
     end
 
     # Expands this section by processing repeats out into actual bars and converting "%" into the previous chord:
     def expanded
-      return self unless repeat
-
       @expanded ||= self.clone.tap do |copy|
         copy.repeat = nil
-        copy.bars = bars * (repeat + 1)
+        copy.bars = bars * (repeat || 0 + 1)
 
         last_bar = nil
         copy.bars.each_with_index do |bar, i|
