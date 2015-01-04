a3a96f9caf70807397bc00e5970ce7f78ac789c2
Ben Hughes <ben@railsgarden.com>
2015-01-04 14:52:56 -0800

    Remove launchrock landing page in production

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -1,9 +1,5 @@
 class DashboardsController < ApplicationController
   def show
-    if Rails.env.production?
-      render :action => "landing", :layout => "landing"
-    else
-      @searchables = Searchable.limit(5).order("RAND()")
-    end
+    @searchables = Searchable.limit(5).order("RAND()")
   end
 end
